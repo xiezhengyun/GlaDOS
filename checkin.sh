af4a1a6d106fb1b182648ab1280f3b166b91bfcd
xiezhengyun <xie_zhengyun@126.com>
2023-11-08 08:59:01 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,12 +1,12 @@
 echo '------------------sign------------------'
 curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/console/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
-curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
+curl -H "cookie:${COOKIE}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
 
 echo '------------------sign------------------'
 curl -H "cookie:${COOKIE2}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/console/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
-curl -H "cookie:${COOKIE2}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
+curl -H "cookie:${COOKIE2}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
 
