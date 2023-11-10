999eabdf59ddff923592162d88f0a3d2d63cc103
xiezhengyun <xie_zhengyun@126.com>
2023-11-10 09:52:45 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,11 +1,11 @@
 echo '------------------sign------------------'
-curl -H "cookie:${COOKIE}" -H "Authorization:6508525923683601371858261555053-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/console/checkin' | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE}" -H "Authorization:6508525923683601371858261555053-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
 
 echo '------------------sign------------------'
-curl -H "cookie:${COOKIE2}" -H "Authorization:6508525923683601371858261555053-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/console/checkin' | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE2}" -H "Authorization:6508525923683601371858261555053-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE2}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
