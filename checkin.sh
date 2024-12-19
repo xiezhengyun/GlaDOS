4b88a46178dbc4e43b107c8df23a0bc4d1da8a31
xiezhengyun <xie_zhengyun@126.com>
2024-12-19 11:02:46 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,5 +1,5 @@
 echo '------------------sign------------------'
-curl -H "cookie:${COOKIE}" -H "Authorization:6508525923683601371858261555053-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE}" -H "Authorization:5054769000972326845543959281021-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
