2e4420f04538cd6323d4c245c3b64fd5208e2a0c
xiezhengyun <xie_zhengyun@126.com>
2025-10-17 08:13:17 +0800

    Update authorization header in checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,5 +1,5 @@
 echo '------------------sign------------------'
-curl -H "cookie:${COOKIE}" -H "Authorization:5054769000972326845543959281021-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE}" -H "24657105248964086372031919083781-1080-1920" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.one"}' -X POST 'https://glados.network/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.network/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
